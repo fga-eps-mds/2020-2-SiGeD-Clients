2453b3f3e56b3fd64a0e2dc098f6304fd3c32b55
lucas <lucascandradex14@gmail.com>
2021-03-25 20:42:04 -0300

    [124] fix eslint
    Co-authored-by: yukioz <yukio.link@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const routes = express.Router();
 const ClientController = require('./Controllers/ClientController');
 
 routes.post('/clients/create', verifyJWT, ClientController.create);
-routes.get('/clients/:id',  verifyJWT, ClientController.access);
+routes.get('/clients/:id', verifyJWT, ClientController.access);
 routes.put('/clients/update/:id', verifyJWT, ClientController.update);
 routes.put('/clients/deactivate/:id', verifyJWT, ClientController.deactivate);
 routes.get('/clients', verifyJWT, ClientController.accessList);
