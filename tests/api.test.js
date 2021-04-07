404953618cdd33d4a2b6471cd48e6f8d102a091f
yukioz <yukio.link@gmail.com>
2021-04-07 17:29:21 -0300

    [170] Adjustment to tests on client API.
    Signed-off-by: yukioz <yukio.link@gmail.com>
    
    Co-authored-by: Davi Marinho <davii_marinho@hotmail.com>

--- tests/api.test.js
+++ tests/api.test.js
@@ -4,14 +4,25 @@ const jwt = require('jsonwebtoken');
 
 describe('Sample Test', () => {
   let id;
+  let falseId;
   const client = {
-    name: 'Davi',
-    cpf: '97844632140',
-    email: 'daviii@gamil.com',
+    name: 'Davi Rogerio',
+    cpf: '17834632140',
+    email: 'davi@gmail.com',
     phone: '988884444',
     office: 'Policial',
     policeStation: 'DPSS',
     city: 'brasília',
+  };
+  const falseClient = {
+    name: 'Bruno',
+    cpf: '17840632140',
+    email: 'brunoTI@gmail.com',
+    phone: '977854444',
+    office: 'Policial',
+    policeStation: 'DPSS',
+    city: 'Ceilândia',
+    active: false,
   };
   const token = jwt.sign({
     name: 'Test',
@@ -30,7 +41,6 @@ describe('Sample Test', () => {
     done();
   });
 
-  // creat client
   it('Post client', async (done) => {
     const res = await request(app).post('/clients/create/').set('x-access-token', token).send(client);
     expect(res.statusCode).toBe(200);
@@ -42,6 +52,21 @@ describe('Sample Test', () => {
     expect(res.body.city).toBe(client.city);
     expect(res.body.office).toBe(client.office);
     id = res.body._id;
+    done();
+  });
+
+  it('Post false client', async (done) => {
+    const res = await request(app).post('/clients/create/').set('x-access-token', token).send(falseClient);
+    expect(res.statusCode).toBe(200);
+    expect(res.body.phone).toBe(falseClient.phone);
+    expect(res.body.cpf).toBe(falseClient.cpf);
+    expect(res.body.name).toBe(falseClient.name);
+    expect(res.body.email).toBe(falseClient.email);
+    expect(res.body.policeStation).toBe(falseClient.policeStation);
+    expect(res.body.city).toBe(falseClient.city);
+    expect(res.body.office).toBe(falseClient.office);
+    expect(res.body.active).toBe(falseClient.active);
+    falseId = res.body._id;
     done();
   });
 
@@ -61,31 +86,30 @@ describe('Sample Test', () => {
     done();
   });
 
-  // get client
   it('Get clients', async (done) => {
     const res = await request(app).get('/clients/').set('x-access-token', token);
     expect(res.statusCode).toBe(200);
-    expect(res.body[0].name).toBe(res.body[0].name);
-    expect(res.body[0].cpf).toBe(res.body[0].cpf);
-    expect(res.body[0].email).toBe(res.body[0].email);
-    expect(res.body[0].phone).toBe(res.body[0].phone);
-    expect(res.body[0].office).toBe(res.body[0].office);
-    expect(res.body[0].policeStation).toBe(res.body[0].policeStation);
-    expect(res.body[0].city).toBe(res.body[0].city);
+    expect(res.body[0].name).toBe(client.name);
+    expect(res.body[0].cpf).toBe(client.cpf);
+    expect(res.body[0].email).toBe(client.email);
+    expect(res.body[0].phone).toBe(client.phone);
+    expect(res.body[0].office).toBe(client.office);
+    expect(res.body[0].policeStation).toBe(client.policeStation);
+    expect(res.body[0].city).toBe(client.city);
     expect(res.body[0].active).toBe(true);
     done();
   });
 
-  it('Get actived clients', async (done) => {
+  it('Get deactivated clients', async (done) => {
     const res = await request(app).get('/clients?active=false').set('x-access-token', token);
     expect(res.statusCode).toBe(200);
-    expect(res.body[0].city).toBe(res.body[0].city);
-    expect(res.body[0].policeStation).toBe(res.body[0].policeStation);
-    expect(res.body[0].email).toBe(res.body[0].email);
-    expect(res.body[0].office).toBe(res.body[0].office);
-    expect(res.body[0].cpf).toBe(res.body[0].cpf);
-    expect(res.body[0].name).toBe(res.body[0].name);
-    expect(res.body[0].phone).toBe(res.body[0].phone);
+    expect(res.body[0].city).toBe(falseClient.city);
+    expect(res.body[0].policeStation).toBe(falseClient.policeStation);
+    expect(res.body[0].email).toBe(falseClient.email);
+    expect(res.body[0].office).toBe(falseClient.office);
+    expect(res.body[0].cpf).toBe(falseClient.cpf);
+    expect(res.body[0].name).toBe(falseClient.name);
+    expect(res.body[0].phone).toBe(falseClient.phone);
     expect(res.body[0].active).toBe(false);
     done();
   });
